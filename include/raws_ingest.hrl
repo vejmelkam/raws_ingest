650218e57a2962db67f1e197297288f9306ba25f
Martin Vejmelka <vejmelkam@gmail.com>
2014-03-10 03:18:35 -0600

    added station_selector() and var_selector() to handle specifications of station lists (explicit list or region lon/lat) and variables

--- include/raws_ingest.hrl
+++ include/raws_ingest.hrl
@@ -1,5 +1,7 @@
 
 -type var_id() :: fm10|air_temp|rel_humidity|soil_temp|pressure|accum_precip|fuel_temp.
+-type station_selector() :: {station_list,[string()]}|{region,{number(),number()},{number(),number()}}.
+-type var_selector() :: all_vars|[var_id()].
 
 -record(raws_obs,{
   timestamp :: calendar:datetime(),  % always in GMT
