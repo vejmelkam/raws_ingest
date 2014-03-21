19ab066aa430ecd96dcc99f4be8c1a801395a169
Martin Vejmelka <vejmelkam@gmail.com>
2014-03-20 19:02:55 -0600

    added write_station_list which dumps a set of station codes to file one per line and stations_to_kml which writes a KML file with the given station info for viewing in GEarth

--- src/raws_export.erl
+++ src/raws_export.erl
@@ -1,9 +1,14 @@
 
 -module(raws_export).
 -author("Martin Vejmelka <vejmelkam@gmail.com>").
--export([obs_to_csv/2,obs_to_csv/3,export_station_data/3]).
+-export([obs_to_csv/2,obs_to_csv/3,export_station_obs/3,export_station_data/1,write_station_list/2,stations_to_kml/2]).
 -include("raws_ingest.hrl").
 
+station_to_csv_string(#raws_station{lat=Lat,lon=Lon,elevation=E}) ->
+  LatS = item_to_list(Lat),
+  LonS = item_to_list(Lon),
+  ES = item_to_list(E),
+  string:join([LatS,LonS,ES],",").
 
 obs_to_csv_string([],_,Lines) ->
   string:join(lists:reverse(Lines),"\n");
@@ -20,16 +25,48 @@ obs_to_csv(Obss,Path) ->
   obs_to_csv(Obss,",",Path).
 
 
-export_station_data(StId,Vars,Yr) ->
+export_station_obs(StId,Vars,Yr) ->
   YrS = integer_to_list(Yr),
   lists:map(fun (V) ->
         Os = raws_ingest:retrieve_observations({station_list,[StId]},[V],{{{Yr,1,1},{0,0,0}},{{Yr+1,1,1},{0,0,0}}}),
         obs_to_csv(Os,lists:flatten([StId,"_",atom_to_list(V),"_",YrS,".csv"])) end, Vars).
 
 
+export_station_data(StId) ->
+  [S] = mnesia:dirty_read(raws_station,StId),
+  file:write_file(lists:flatten([StId,"_data.csv"]),station_to_csv_string(S)).
+
+
+write_station_list(Ss,Path) ->
+  file:write_file(Path,string:join(Ss,"\n")).
+
+
+stations_to_kml(Sinfos,Path) ->
+  KML = [
+    "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n",
+    "<kml xmlns=\"http://earth.google.com/kml/2.1\">\n",
+    "<Document>\n",
+    "  <name>Stations exported from raws_ingest</name>\n",
+    "<Folder>\n",
+    "<name>RAWS</name>\n",
+    lists:map(fun station_info_to_kml/1, Sinfos),
+    "</Folder>\n",
+    "</Document>\n",
+    "</kml>\n"],
+  file:write_file(Path,lists:flatten(KML)).
+
+
 %% ----------------------------------------
 %% Internal functions
 %% ----------------------------------------
+
+station_info_to_kml(#raws_station{id=I,name=N,lat=Lat,lon=Lon,elevation=Elev}) ->
+  [LonS,LatS,ElevS] = lists:map(fun (X) -> io_lib:format("~p", [X]) end, [Lon,Lat,Elev]),
+  [ "<Placemark>\n",
+    "  <name>",I,"</name>\n",
+    "  <Point><coordinates>",LonS,",",LatS,",0</coordinates></Point>\n",
+    "  <description><![CDATA[ <b>Name:</b> ",N,"<br><b>Lat/Lon:</b>",LatS,",",LonS,"<br><b>Elevation:</b>",ElevS,"]]></description>\n"
+    "</Placemark>\n"].
 
 
 item_to_list(N) when is_integer(N) ->
